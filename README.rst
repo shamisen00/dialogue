0edaa267cff62f6ed4808aaf81292c5ad00caa87
yuki.kobayashi <ykobayashi134@gmail.com>
2023-01-24 05:04:31 +0000

    Add server functionality

--- README.rst
+++ README.rst
@@ -3,3 +3,6 @@ poetry install
 # activate pre-commit
 
 pre-commit install
+
+# start server
+uvicorn demo.test:app
--- demo/test.py
+++ demo/test.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
+
 
 @app.get("/hello")
 async def hello():
     return {"message": "hello world!"}
+
+
+def main():
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+
+
+if __name__ == "__main__":
+    main()
