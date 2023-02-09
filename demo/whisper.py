2606991dfb524381e9c996f407fa2872afc324f3
yuki.kobayashi <ykobayashi134@gmail.com>
2023-02-09 14:42:07 +0000

    modified:   demo/whisper.py

--- demo/whisper.py
+++ demo/whisper.py
@@ -1,4 +1,9 @@
+import whisper
 import soundcard as sc
+import threading
+import queue
+import numpy as np
+import argparse
 
 with sc.get_microphone(id=str(sc.default_speaker().name), include_loopback=True).recorder(samplerate=SAMPLE_RATE, channels=1) as mic:
     while True:
