68cbd38ec221224aebf4bd83995c0f9ad885344a
yuki.kobayashi <ykobayashi134@gmail.com>
2023-02-11 14:38:55 +0000

    update

--- demo/whisper.py
+++ demo/whisper.py
@@ -5,6 +5,10 @@ import queue
 import numpy as np
 import argparse
 
+SAMPLE_RATE = 16000
+INTERVAL = 3
+BUFFER_SIZE = 4096
+
 with sc.get_microphone(id=str(sc.default_speaker().name), include_loopback=True).recorder(samplerate=SAMPLE_RATE, channels=1) as mic:
     while True:
         data = mic.record(BUFFER_SIZE)
